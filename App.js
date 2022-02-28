88ccdff5f352d35a18cfc689df854bd675046579
duru <zenishdhakal@gmail.com>
2022-02-28 11:29:44 +0545

    Screen added

--- App.js
+++ App.js
@@ -1,14 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Screen/LoginScreen';
+import Dashboard from "./Screen/Dashboard";
+import icon from "./assets/icon.png";
 
+import SignUp from './Screen/SignUp';
+import HomeScreenData from "./Screen/Homescreen";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [isLoading,setLoading]= useState(true);
+
+  useEffect(()=>{
+    
+    setTimeout(()=>{
+       setLoading(false);
+    },2000);
+  },[]);
+
+  if(isLoading){
+    return(
+      
+    <View style={{flex:1,backgroundColor:"#34568B"}} >
+    <HomeScreenData />
+    
+  
     </View>
+   
+     )}
+     
+
+  const stack = createStackNavigator();
+  return (
+   
+// 
+
+     <NavigationContainer>
+
+     
+     <stack.Navigator >
+     <stack.Screen name="LoginScreen" component={LoginScreen}  />
+     <stack.Screen name="signup" component={SignUp}   />
+     <stack.Screen name="Dashboard" component={Dashboard} options={{headerShown:false}} />
+     </stack.Navigator>
+     </NavigationContainer>
+
+   
+      
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
